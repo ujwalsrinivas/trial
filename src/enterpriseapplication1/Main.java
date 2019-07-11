d069b11a37da115f1305239349cb0df8240bdfe4
1992853 <1992853@E20754.CEGEPGIM.LCL>
2019-07-11 15:52:05 -0400

    additioncommit

---
+++ src/enterpriseapplication1/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enterpriseapplication1;
+
+/**
+ *
+ * @author 1992853
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        // TODO code application logic here
+    }
+    
+}
